44b7254bf2ef45a292f68749ed1050cff2e7645a
Drew Manlove <drewmanlove@Drews-MBP.domain>
2016-06-09 14:49:11 -0600

    save

--- create-stack.sh
+++ create-stack.sh
@@ -13,7 +13,7 @@ aws cloudformation create-stack \
 	--stack-name $UUID \
 	--template-body file://cloud-formation.json \
 	--parameters \
-		ParameterKey=KeyName,ParameterValue=default \
+		ParameterKey=KeyName,ParameterValue=brent-keys2 \
 		ParameterKey=ArtifactoryPassword,ParameterValue=$5 \
 		ParameterKey=BrokerCapacity,ParameterValue=$4 \
 		ParameterKey=AgilDataVersion,ParameterValue=$3 \
